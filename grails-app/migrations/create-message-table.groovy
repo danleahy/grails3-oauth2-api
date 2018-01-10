b9d1bb3b8bae6d995bd9fe903d7e4ca8eb323fe9
Daniel Leahy <danleahy@gmail.com>
2018-01-10 14:36:28 +0000

    changed message table message type to postgresql type text to handle large json strings

--- grails-app/migrations/create-message-table.groovy
+++ grails-app/migrations/create-message-table.groovy
@@ -1,16 +1,16 @@
 databaseChangeLog = {
 
-    changeSet(author: "Daniel (generated)", id: "1515515560825-1") {
+    changeSet(author: "Daniel (generated)", id: "1515581497870-1") {
         createSequence(sequenceName: "hibernate_sequence")
     }
 
-    changeSet(author: "Daniel (generated)", id: "1515515560825-2") {
+    changeSet(author: "Daniel (generated)", id: "1515581497870-2") {
         createTable(tableName: "message") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(primaryKey: "true", primaryKeyName: "messagePK")
             }
 
-            column(name: "message", type: "VARCHAR(255)")
+            column(name: "message", type: "CLOB")
 
             column(name: "date_created", type: "timestamp")
 
